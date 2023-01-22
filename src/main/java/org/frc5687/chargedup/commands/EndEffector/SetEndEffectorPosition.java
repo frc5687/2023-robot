9be6b515bac7039a2b089d192df51383499f1748
Linus Krenkel <114187942+SpeakerForTheMeese@users.noreply.github.com>
2023-01-22 13:23:14 -0500

    Fix seperation of end effector
    -fix so command so that it uses _endEffector instead of _wrist and _gripper

--- src/main/java/org/frc5687/chargedup/commands/EndEffector/SetEndEffectorPosition.java
+++ src/main/java/org/frc5687/chargedup/commands/EndEffector/SetEndEffectorPosition.java
@@ -8,41 +8,42 @@ import edu.wpi.first.math.util.Units;
 
 public class SetEndEffectorPosition extends OutliersCommand{
     
-    private EndEffector _wrist;
-    private EndEffector _gripper;
+    private EndEffector _endEffector;
+    
     
     private double _wristAngleDegrees;
     private double _gripperAngleDegrees;
 
-    public SetEndEffectorPosition(EndEffector wrist, EndEffector gripper,
+    public SetEndEffectorPosition(EndEffector endEffector,
      double wristAngleDegrees, double gripperAngleDegrees ) {
-        _wrist = wrist;
-        _gripper = gripper;
+       _endEffector = endEffector;
 
         _wristAngleDegrees = wristAngleDegrees;
         _gripperAngleDegrees = gripperAngleDegrees;
-        addRequirements(_wrist, gripper);
+        addRequirements(_endEffector);
      }
      @Override
      public void initialize() {
          // TODO Auto-generated method stub
          super.initialize();
-         _wrist.setWristSetpointDegrees(_wristAngleDegrees);
-         _gripper.setGripperSetpointDegrees(_gripperAngleDegrees);
+         _endEffector.setWristSetpointDegrees(_wristAngleDegrees);
+         _endEffector.setGripperSetpointDegrees(_gripperAngleDegrees);
      }
      @Override
      public void execute() {
          // TODO Auto-generated method stub
          super.execute();
-         double wristOutput = _wrist.getWristControllerOutput();
-         double gripperOutput = _gripper.getGripperControllerOutput();
-         _wrist.setWristSpeed(wristOutput);
-         _gripper.setGripperSpeed(gripperOutput);
+         double wristOutput = _endEffector.getWristControllerOutput();
+         double gripperOutput = _endEffector.getGripperControllerOutput();
+         _endEffector.setWristSpeed(wristOutput);
+         _endEffector.setGripperSpeed(gripperOutput);
      }
      @Override
      public boolean isFinished() {
          // TODO Auto-generated method stub
-         return Math.abs(Units.degreesToRadians(_wristAngleDegrees)- _wrist.getWristAngleRadians()) < Constants.EndEffector.WRIST_TOLERENCE &&  Math.abs(Units.degreesToRadians(_gripperAngleDegrees) - _gripper.getGripperAngleRadians()) < Constants.EndEffector.GRIPPER_TOLERENCE;
+         return
+          Math.abs(Units.degreesToRadians(_wristAngleDegrees)- _endEffector.getWristAngleRadians()) < Constants.EndEffector.WRIST_TOLERENCE 
+          &&  Math.abs(Units.degreesToRadians(_gripperAngleDegrees) - _endEffector.getGripperAngleRadians()) < Constants.EndEffector.GRIPPER_TOLERENCE;
         }
      @Override
      public void end(boolean interrupted) {
