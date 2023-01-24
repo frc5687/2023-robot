be1d8e5de2deee73d6f394b865d46d9e6337ef91
SuperSaurus1 <78873930+SuperSaurus1@users.noreply.github.com>
2023-01-23 23:06:59 -0500

    Setting up constructor and basic class.

--- src/main/java/org/frc5687/chargedup/subsystems/ExtendingArm.java
+++ src/main/java/org/frc5687/chargedup/subsystems/ExtendingArm.java
@@ -1,8 +1,12 @@
 package org.frc5687.chargedup.subsystems;
 
+import org.frc5687.chargedup.Constants;
 import org.frc5687.chargedup.util.OutliersContainer;
 import org.frc5687.lib.drivers.OutliersTalon;
 import org.frc5687.lib.sensors.HallEffect;
+
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class ExtendingArm extends OutliersSubsystem {
     private OutliersTalon _talon;
@@ -10,7 +14,21 @@ public class ExtendingArm extends OutliersSubsystem {
 
     public ExtendingArm(OutliersContainer container){
         super(container);
+        _talon = new OutliersTalon talon(0, Constants.ExtendingArm.CAN_BUS, "ExtendingArm");
+        talon.configure(Constants.ExtendingArm.CONFIG);
     } 
+
+    public void setArmSpeed(double speed) {
+        _talon.set(ControlMode.PercentOutput, speed);
+    }
+
+    public boolean getHall() {
+        return _hall.get();
+    }
+
+    public double getEncoderTicks() {
+        return _talon.getSelectedSensorPosition();
+    }
 
     public void updateDashboard() {}
 }
