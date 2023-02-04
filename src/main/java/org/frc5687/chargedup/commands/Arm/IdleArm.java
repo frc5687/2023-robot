0f0f9f792dc8d0d7360240b672ec94b9f7b157f0
deslobodzian <de.slobodzian@gmail.com>
2023-02-04 08:44:41 -0500

    Added IdleArm command that hold the angle of the arm from its last state.

---
+++ src/main/java/org/frc5687/chargedup/commands/Arm/IdleArm.java
@@ -0,0 +1,35 @@
+package org.frc5687.chargedup.commands.Arm;
+
+import org.frc5687.chargedup.commands.OutliersCommand;
+import org.frc5687.chargedup.subsystems.Arm;
+
+/* This is to hold the last arm state sent to the controller. */
+public class IdleArm extends OutliersCommand {
+    private final Arm _arm;
+    public IdleArm(Arm arm) {
+        _arm = arm;
+        addRequirements(_arm);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        _arm.setNextReference(_arm.getLastState());
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+        _arm.setArmVoltage(_arm.getNextVoltage());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+}
