04b8784feb0586a00ca7e0492d3b11001bdcf9a5
jackellingwood <jack.ellingwood@baxter-academy.org>
2023-02-16 17:22:44 -0500

    slow mode and place speedup

--- src/main/java/org/frc5687/chargedup/Constants.java
+++ src/main/java/org/frc5687/chargedup/Constants.java
@@ -55,7 +55,9 @@ public class Constants {
         public static final double SWERVE_WE_POS = WIDTH / 2.0;
 
         public static final double MAX_MPS = 5.0; // Max speed of robot (m/s)
+        public static final double SLOW_MPS = 2.5; // Slow speed of robot (m/s)
         public static final double MAX_ANG_VEL = Math.PI * 2.0; // Max rotation rate of robot (rads/s)
+        public static final double SLOW_ANG_VEL = Math.PI * 1.0; // Max rotation rate of robot (rads/s)
         public static final KinematicLimits KINEMATIC_LIMITS = new KinematicLimits();
         static {
             KINEMATIC_LIMITS.maxDriveVelocity = 5.3; // m/s
@@ -351,7 +353,7 @@ public class Constants {
         //fully closed angle
         public static final double GRIPPER_IN_SPEED = -0.5;
         // fully open angle
-        public static final double GRIPPER_OUT_SPEED = 0.5;
+        public static final double GRIPPER_OUT_SPEED = 1.0;
         //public static final double GRIPPER_CUBE_ANGLE = Units.degreesToRadians(186.0);
         public static final boolean  GRIPPPER_INVERTED = false;
 
