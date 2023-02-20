0cb5b75bf0702306e501c31d49d5b8eb90cab186
SuperSaurus1 <78873930+SuperSaurus1@users.noreply.github.com>
2023-02-20 16:33:58 -0500

    Hard coded pick up because oof code design. :(

--- src/main/java/org/frc5687/chargedup/commands/AutoSetSuperStructurePosition.java
+++ src/main/java/org/frc5687/chargedup/commands/AutoSetSuperStructurePosition.java
@@ -27,5 +27,5 @@ public class AutoSetSuperStructurePosition extends SequentialCommandGroup {
             )
         );
     }
-    
+
 }
--- src/main/java/org/frc5687/chargedup/commands/SemiAuto/SemiAutoPickupCone.java
+++ src/main/java/org/frc5687/chargedup/commands/SemiAuto/SemiAutoPickupCone.java
@@ -5,7 +5,10 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.frc5687.chargedup.Constants;
 import org.frc5687.chargedup.OI;
 import org.frc5687.chargedup.commands.AutoSetSuperStructurePosition;
+import org.frc5687.chargedup.commands.Arm.AutoSetArmSetpoint;
 import org.frc5687.chargedup.commands.Arm.HoldArm;
+import org.frc5687.chargedup.commands.Elevator.AutoExtendElevator;
+import org.frc5687.chargedup.commands.EndEffector.AutoSetRollerSpeed;
 import org.frc5687.chargedup.commands.EndEffector.AutoSetWristAngle;
 import org.frc5687.chargedup.commands.EndEffector.CloseConeGripper;
 import org.frc5687.chargedup.commands.EndEffector.WaitForManualGripper;
@@ -26,15 +29,13 @@ public class SemiAutoPickupCone extends SequentialCommandGroup {
                 // new DriveUntilInHall(elevator),
                // new AutoExtendElevator(elevator, 0.01),
                 new AutoSetWristAngle(endEffector, Constants.EndEffector.WRIST_SAFE_ANGLE),
-                new AutoSetSuperStructurePosition(
-                        elevator, endEffector, arm, setpoint
-                ),
+                new AutoExtendElevator(elevator, setpoint.elevatorPosition),
+                new ParallelDeadlineGroup(new AutoSetArmSetpoint(arm, setpoint.armAngle), new AutoSetRollerSpeed(endEffector, setpoint.gripperSpeed, isFinished())),
                 new ParallelDeadlineGroup(new WaitForManualGripper(endEffector, oi, false), new HoldArm(arm, setpoint.armAngle)),
 
 //                new CloseConeGripper(endEffector),
-                new AutoSetSuperStructurePosition(
-                        elevator, endEffector, arm, idleConeSetpoint
-                )
+                new AutoSetWristAngle(endEffector, idleConeSetpoint.wristAngle),
+                new ParallelDeadlineGroup(new AutoSetArmSetpoint(arm, idleConeSetpoint.armAngle), new AutoSetRollerSpeed(endEffector, idleConeSetpoint.gripperSpeed, isFinished()))
         );
     }
 }
