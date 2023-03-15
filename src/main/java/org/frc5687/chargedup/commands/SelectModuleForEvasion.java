7d74008ff371dbd7efa094b64c6ae9aa651db81f
SuperSaurus1 <simeon.pillsbury@gmail.com>
2023-03-15 15:46:35 -0400

    Creating new command for evasive maneuvers :)

---
+++ src/main/java/org/frc5687/chargedup/commands/SelectModuleForEvasion.java
@@ -0,0 +1,9 @@
+package org.frc5687.chargedup.commands;
+
+import org.frc5687.chargedup.subsystems.DriveTrain;
+
+public class SelectModuleForEvasion extends OutliersCommand{
+    private final DriveTrain _driveTrain;
+
+    public SelectModuleForEvasion(DriveTrain _driveTrain,)
+}
