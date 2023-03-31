4447c92983bac19e7ac3683cca830c50fd7f7611
jackellingwood <jack.ellingwood@baxter-academy.org>
2023-03-31 09:43:27 -0400

    separating rotation and extension, untested

--- src/main/java/org/frc5687/chargedup/commands/AutoSetSuperStructurePosition.java
+++ src/main/java/org/frc5687/chargedup/commands/AutoSetSuperStructurePosition.java
@@ -1,6 +1,8 @@
 package org.frc5687.chargedup.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import org.frc5687.chargedup.commands.Arm.AutoSetArmSetpoint;
 import org.frc5687.chargedup.commands.Elevator.AutoExtendElevator;
 import org.frc5687.chargedup.commands.EndEffector.SetEndEffectorPosition;
@@ -9,12 +11,15 @@ import org.frc5687.chargedup.subsystems.Elevator;
 import org.frc5687.chargedup.subsystems.EndEffector;
 import org.frc5687.chargedup.util.SuperStructureSetpoints.Setpoint;
 
-public class AutoSetSuperStructurePosition extends ParallelCommandGroup {
+public class AutoSetSuperStructurePosition extends SequentialCommandGroup {
     public AutoSetSuperStructurePosition(
             Elevator elevator, EndEffector endEffector, Arm arm, Setpoint setpoint) {
         addCommands(
-                new AutoSetArmSetpoint(arm, setpoint.armAngle),
+            new AutoSetArmSetpoint(arm, setpoint.armAngle),
+            new ParallelCommandGroup(
                 new SetEndEffectorPosition(endEffector, setpoint.wristAngle, setpoint.gripperSpeed),
-                new AutoExtendElevator(elevator, setpoint.elevatorPosition));
+                new AutoExtendElevator(elevator, setpoint.elevatorPosition)
+            )
+        );
     }
 }
