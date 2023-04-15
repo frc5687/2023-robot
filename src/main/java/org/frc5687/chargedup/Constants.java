70b4ada149bd5ab0e0b100c06110115d45aaedd8
jackellingwood <jack.ellingwood@baxter-academy.org>
2023-04-15 14:23:07 -0400

    change place angle and 1.5 piece auto

--- src/main/java/org/frc5687/chargedup/Constants.java
+++ src/main/java/org/frc5687/chargedup/Constants.java
@@ -377,7 +377,7 @@ public class Constants {
         // Positive is North, Negative is South (North is towards Battery)
         public static final double ANGLE_TOLERANCE = Units.degreesToRadians(1.0); // rads
         public static final double VERTICAL_ARM_ANGLE = 1.2; // rads
-        public static final double PLACE_ARM_ANGLE = 0.233; // testing
+        public static final double PLACE_ARM_ANGLE = 0.25; // testing
 
         public static final OutliersTalon.ClosedLoopConfiguration CLOSED_LOOP_CONFIGURATION = new OutliersTalon.ClosedLoopConfiguration();
         static {
